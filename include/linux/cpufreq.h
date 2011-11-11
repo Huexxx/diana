fa4e9eb593a7b33e2aec4f36bb8f947560f58faa
Javier Vazquez <huexxx@gmail.com>
2011-11-11 12:09:27 +0100

    Add MinMax Governor tweaked for P970

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -378,6 +378,9 @@ extern struct cpufreq_governor cpufreq_gov_smartass2;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_INTERACTIVEX)
 extern struct cpufreq_governor cpufreq_gov_interactivex;
 #define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_interactivex)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_MINMAX)
+extern struct cpufreq_governor cpufreq_gov_minmax;
+#define CPUFREQ_DEFAULT_GOVERNOR       (&cpufreq_gov_minmax)
 #endif
 
 
