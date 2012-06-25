ccb09cc95261b66290c83e6da01c2bebc2c7ce4b
Huexxx <huexxx@gmail.com>
2012-06-25 13:47:53 +0200

    Changes in PM to always respect cpufreq policy limits

--- arch/arm/plat-omap/omap-pm.c
+++ arch/arm/plat-omap/omap-pm.c
@@ -586,8 +586,44 @@ void omap_pm_dsp_set_min_opp(u8 opp_id)
 	
 	if (dsp_freq_table[dsp_req_id].frequency >
 		dsp_freq_table[mpu_req_id].frequency) {
+		/*
+		 * Ensure that this request is not conflicting with cpufreq
+		 * constraints. If that is the case, cpufreq wins.
+		 */
+		struct cpufreq_policy policy;
+
+		cpufreq_get_policy(&policy, 0);
+
+		while (mpu_freq_table[dsp_req_id].frequency < policy.min && dsp_req_id < ft_count - 1) {
+			dsp_req_id = dsp_req_id + 1;
+		}
+		while (mpu_freq_table[dsp_req_id].frequency > policy.max && dsp_req_id > 0) {
+			dsp_req_id = dsp_req_id - 1;
+		}
+		/*
+		 * cpufreq check end
+		 */
+
 		selopp = dsp_req_id;
 	} else {
+		/*
+		 * Ensure that this request is not conflicting with cpufreq
+		 * constraints. If that is the case, cpufreq wins.
+		 */
+		struct cpufreq_policy policy;
+
+		cpufreq_get_policy(&policy, 0);
+
+		while (mpu_freq_table[mpu_req_id].frequency < policy.min && mpu_req_id < ft_count - 1) {
+			mpu_req_id = mpu_req_id + 1;
+		}
+		while (mpu_freq_table[mpu_req_id].frequency > policy.max && mpu_req_id > 0) {
+			mpu_req_id = mpu_req_id - 1;
+		}
+		/*
+		 * cpufreq check end
+		 */
+
 		selopp = mpu_req_id;
 	}
 
@@ -819,10 +855,24 @@ int omap_pm_set_min_mpu_freq(struct device *dev, unsigned long f)
 	/* Save the current constraint */
 	old_max_level = mpu_tput->max_level;
 
-	if (f == -1)
+	if (f == -1) {
 		remove_req_tput(dev, mpu_tput);
-	else
+	} else {
+		struct cpufreq_policy policy;
+
+		cpufreq_get_policy(&policy, 0);
+
+		/*
+		* Ensure that this request is not conflicting with cpufreq
+		* constraints. If that is the case, cpufreq wins.
+		*/
+		if ((f/1000) < policy.min)
+			f = policy.min * 1000;
+		if ((f/1000) > policy.max)
+			f = policy.max * 1000;
+
 		add_req_tput(dev, f, mpu_tput);
+	}
 
 	/* Find max constraint after the operation */
 	max_lookup(mpu_tput);
